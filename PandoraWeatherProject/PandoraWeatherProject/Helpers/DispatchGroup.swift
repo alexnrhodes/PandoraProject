01b8772980832a009f6217aaf87c372cdabc041f
Alexandra Rhodes <anrhodes5738@gmail.com>
2020-03-02 16:00:16 -0600

    Add network calls + Dispatch Group

---
+++ PandoraWeatherProject/PandoraWeatherProject/Helpers/DispatchGroup.swift
@@ -0,0 +1,13 @@
+//
+//  DispatchGroup.swift
+//  PandoraWeatherProject
+//
+//  Created by Alex Rhodes on 3/1/20.
+//  Copyright © 2020 Alexandra Rhodes. All rights reserved.
+//
+
+import Foundation
+
+class Group {
+    static let dispatchGroup: DispatchGroup = DispatchGroup()
+}
